889329e2e7f6d8905cd0945343eea4b68b04e5b7
Jed.Lin <jed.lin@higgstar.com>
2024-11-07 17:52:31 +0800

    fixed: Change the access modifier of the GenericResponse class from internal to public

--- NetShape/NetShape/Core/Models/GenericResponse.cs
+++ NetShape/NetShape/Core/Models/GenericResponse.cs
@@ -1,6 +1,6 @@
 namespace NetShape.Core.Models;
 
-internal class GenericResponse<T>: IResponse<T>
+public class GenericResponse<T>: IResponse<T>
 {
     public string RequestId { get; set; }
     public T Data { get; set; }
